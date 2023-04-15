40bb5815f9cd1b013b5ea648cdc8a68437218e2c
roser42 <ar.seredenko@gmail.com>
2023-04-15 04:02:47 +0400

    [DEV] add man noDataIcon to blocker page

--- src/pages/SiteBlockerPage/AddressesList.tsx
+++ src/pages/SiteBlockerPage/AddressesList.tsx
@@ -1,11 +1,13 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useRef, useState } from "react";
 import type { BlockedAddress } from "~app/types/BlockedAddress";
 import { classNames } from "~shared/lib/classNames/classNames";
 import styles from "~pages/SiteBlockerPage/SiteBlocker.module.scss";
 import { AddressBlocker } from "~widgets/AddressBlocker/AddressBlocker";
 import { newBlockedAddress } from "~app/types/BlockedAddress";
-import { removeAddress, updateAddress } from "~app/reducers/blockedAddresses-slice";
+import { blockAll, removeAddress, updateAddress } from "~app/reducers/blockedAddresses-slice";
 import { useAppDispatch, useAppSelector } from "~store";
+import { BlockPageManIcon } from "~shared/resources/icons/people/BlockPageManIcon";
+import { Button, ThemeButton } from "~shared/ui/Button/Button";
 
 type AddressesListProps = {
     className?: string
@@ -15,6 +17,10 @@ export const AddressesList: FC<AddressesListProps> = (props) => {
     const dispatch = useAppDispatch();
     const addresses = useAppSelector((state) => state.blockedAddresses);
 
+    function handleBlockAllButtonClick() {
+        dispatch(blockAll(!addresses.allBlocked));
+    }
+
     function handleBlock(addr: BlockedAddress) {
         dispatch(updateAddress(addr));
     }
@@ -23,20 +29,50 @@ export const AddressesList: FC<AddressesListProps> = (props) => {
         dispatch(removeAddress(addr.addr));
     }
 
-    console.log('from list')
+    const _blockAllButton = () => {
+        return (
+            <Button
+                className={classNames(styles.blockAll)}
+                theme={ThemeButton.CLEAR}
+                onClick={handleBlockAllButtonClick}
+            >
+                {addresses.allBlocked ? "Unlock all" : "Block all"}
+            </Button>
+        );
+    };
+
+    const _addressesList = () => {
+        return (
+            <>
+                {addresses.addresses.map((addr, index) => {
+                    return (
+                        <AddressBlocker
+                            className={""}
+                            key={addr.addr}
+                            address={newBlockedAddress(addr.addr, addr.blocked)}
+                            OnBlock={handleBlock}
+                            OnDelete={handleDelete}
+                        />
+                    );
+                })}
+            </>
+        );
+    };
+
+    const _noDataIcon = () => {
+        return (
+            <div className={classNames(styles.noDataIconContainer)}>
+                <BlockPageManIcon />
+            </div>
+        );
+    };
 
     return (
-        <div className={classNames(styles.addressesContainer, {}, [props.className])}>
-            {addresses.addresses.map(addr => {
-                return (
-                    <AddressBlocker
-                        key={addr.addr}
-                        address={newBlockedAddress(addr.addr, addr.blocked)}
-                        OnBlock={handleBlock}
-                        OnDelete={handleDelete}
-                    />
-                );
-            })}
-        </div>
+        <>
+            {addresses.addresses.length !== 0 ? <_blockAllButton /> : <></>}
+            <div className={classNames(styles.addressesContainer, {}, [props.className])}>
+                {addresses.addresses.length !== 0 ? <_addressesList /> : <_noDataIcon />}
+            </div>
+        </>
     );
 };
--- src/pages/SiteBlockerPage/SiteBlockerPage.tsx
+++ src/pages/SiteBlockerPage/SiteBlockerPage.tsx
@@ -4,24 +4,16 @@ import { Button, ThemeButton } from "~shared/ui/Button/Button";
 import { AddressBlocker } from "~widgets/AddressBlocker/AddressBlocker";
 import { AddNewAddress } from "~pages/SiteBlockerPage/AddNewAddress";
 import type { BlockedAddress } from "~app/types/BlockedAddress";
-import { newBlockedAddress } from "~app/types/BlockedAddress";
-import { addAddress, blockAll, removeAddress, updateAddress } from "~app/reducers/blockedAddresses-slice";
+import { addAddress, blockAll } from "~app/reducers/blockedAddresses-slice";
 import { useAppDispatch, useAppSelector } from "~store";
 import { AddressesList } from "~pages/SiteBlockerPage/AddressesList";
 
 const SiteBlockerPage = () => {
     const dispatch = useAppDispatch();
-    const allBlocked = useAppSelector((state) => state.blockedAddresses.allBlocked);
-
-    function handleBlockAllButtonClick() {
-        dispatch(blockAll(!allBlocked));
-    }
 
     function handleAddNewAddress(addr: BlockedAddress) {
         dispatch(addAddress(addr));
     }
-
-    console.log(allBlocked, "from page");
 
     return (
         <div className={classNames(styles.layout)}>
@@ -32,13 +24,7 @@ const SiteBlockerPage = () => {
                 Block sites that interfere with your productivity
             </span>
             <AddNewAddress OnAdd={handleAddNewAddress} />
-            <Button
-                className={classNames(styles.blockAll)}
-                theme={ThemeButton.CLEAR}
-                onClick={handleBlockAllButtonClick}
-            >
-                {allBlocked ? "Unlock all" : "Block all"}
-            </Button>
+
             <AddressesList />
         </div>
     );
