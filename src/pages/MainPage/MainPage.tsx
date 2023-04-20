a54298ee6649c1b524c68126ca4ec16dc628f3f2
Александр Крюков <sashakryukovoff@mail.ru>
2023-04-21 01:32:09 +0400

    dev

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -39,12 +39,12 @@ const MainPage = () => {
             <GuideModal guideModalActive={guideModalActive}
                         setGuideModalActive={setGuideModalActive}
             />
-            {!showWelcome
-                ??
-              <WelcomeModal
-                welcomeModalActive={welcomeModalActive}
-                setWelcomeModalActive={setWelcomeModalActive}
-              />
+            {!showWelcome ?
+                <WelcomeModal
+                    welcomeModalActive={welcomeModalActive}
+                    setWelcomeModalActive={setWelcomeModalActive}
+                />
+                : <></>
             }
         </div>
     );
