a32ef49d87cb383dfec0f909819768b2a5171460
roser42 <ar.seredenko@gmail.com>
2023-04-24 00:22:05 +0400

    [DEV] change dot icon stroke color

--- src/shared/resources/icons/DotIcon.tsx
+++ src/shared/resources/icons/DotIcon.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const DotIcon = (props: { color: string }) => {
     return (
         <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="4" cy="4" r="3.5" fill={props.color} stroke="black" />
+            <circle cx="4" cy="4" r="3.5" fill={props.color} stroke={"#26282B"} />
         </svg>
     );
 };
