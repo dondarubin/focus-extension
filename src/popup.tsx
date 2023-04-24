afb63b21552b9fe662d2ac4f65819a9972337b1f
roser42 <ar.seredenko@gmail.com>
2023-04-24 13:57:01 +0400

    [DEV] create useOverlay hook for drowdown

--- src/popup.tsx
+++ src/popup.tsx
@@ -7,16 +7,11 @@ import { PersistGate } from "@plasmohq/redux-persist/es/integration/react";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { DotLoader } from "~shared/ui/Loaders/DotLoader/DotLoader";
-import { _tempControls } from "~widgets/_tempControls/_tempControls";
-
 
 function IndexPopup() {
     return (
         <Provider store={store}>
-            <PersistGate persistor={persistor} loading={
-                <DotLoader />
-            }
-            >
+            <PersistGate persistor={persistor} loading={<DotLoader />}>
                 <ThemeProvider>
                     <ToastContainer />
                     <App />
