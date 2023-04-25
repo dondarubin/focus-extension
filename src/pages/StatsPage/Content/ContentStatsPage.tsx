016124e079d4fdb30ecb6d8fd61b2e13ee1ade84
Александр Крюков <sashakryukovoff@mail.ru>
2023-04-25 23:33:06 +0400

    [DEV] dev stats page

---
+++ src/pages/StatsPage/Content/ContentStatsPage.tsx
@@ -0,0 +1,40 @@
+import styles from "./ContentStatsPage.module.scss";
+import { classNames } from "~shared/lib/classNames/classNames";
+import { FlowTrackerChart } from "~widgets/FlowTrakerChart/FlowTrackerChart";
+import React from "react";
+
+interface ContentStatsPageProps {
+    className?: string;
+}
+
+const reward = () => {
+    return (
+        <div className={styles.userTask}>
+            <div className={styles.nav}>
+                <span className={styles.countStars}>14</span>
+            </div>
+
+            <div className={styles.bar}></div>
+        </div>
+    );
+};
+
+export const ContentStatsPage = ({ className }: ContentStatsPageProps) => {
+    return (
+        <div className={classNames(styles.ContentStatsPage, {}, [className])}>
+            <div className={styles.contentWrapper}>
+                <FlowTrackerChart />
+
+                <div className={styles.userTasks}>
+                    {reward()}
+                    {reward()}
+                    {reward()}
+                    {reward()}
+                    {reward()}
+                    {reward()}
+                    {reward()}
+                </div>
+            </div>
+        </div>
+    );
+};
--- src/widgets/FlowTrakerChart/FlowTrackerChart.tsx
+++ src/widgets/FlowTrakerChart/FlowTrackerChart.tsx
@@ -13,8 +13,14 @@ export const FlowTrackerChart: FC<FlowTrackerChartProps> = (props) => {
     } = props;
 
     return (
-        <div className={classNames(styles.FlowTrackerChart, {}, [])}>
-            <BarChart data={[3, 1, 5, 2, 3, 1, 13]}></BarChart>
+        <div className={classNames(styles.FlowTrackerChart, {}, [className])}>
+            <div className={styles.info}>
+                <span className={styles.hours}>23 Hours</span>
+                <span className={styles.week}>For the last week</span>
+            </div>
+            <div className={styles.bar}>
+                <BarChart data={[3, 1, 5, 2, 3, 1, 13]}></BarChart>
+            </div>
         </div>
     );
 };
